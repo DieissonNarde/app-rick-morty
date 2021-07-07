786157ee94c95d0143e015189426f38f3b88b72a
DieissonNarde <dieissonnarde@gmail.com>
2021-07-07 16:32:25 -0400

    :sparkles:  Implement list of favorite characters

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,11 +1,43 @@
+import { useEffect, useState } from "react";
 
-import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
+import { Card } from "../../components/Card";
+import { Footer } from "../../components/Footer";
 
-import { FavoritesContainer } from "./styles";
+import { FavoritesContainer, LoadingDiv, NotFavoritesDiv } from "./styles";
 import homeImg from '../../assets/images/home.svg';
+import notFavoritesImg from '../../assets/images/not-favorites.ico';
+
+import { useFavorites } from "../../hooks/useFavorites";
+
+type Character = {
+  id: number;
+  image: string;
+  name: string;
+  species: string;
+  type?: string;
+}
 
 export function Favorites(): JSX.Element {
+  const [loading, setLoading] = useState(true);
+  const [notFavorite, setNotFavorite] = useState(false);
+
+  const { favorites } = useFavorites();
+
+  useEffect(() => {
+    async function loadFavorites() {
+      if (favorites.length >= 1) {
+        setLoading(false);
+        setNotFavorite(false);
+      } else {
+        setLoading(false);
+        setNotFavorite(true);
+      }
+    }
+
+    loadFavorites();
+  }, [favorites])
+
   return (
     <div>
       <Header imgButton={homeImg} toLink={"/"}>
@@ -13,9 +45,24 @@ export function Favorites(): JSX.Element {
       </Header>
 
       <FavoritesContainer>
-        <h1>Sua lista de Favoritos</h1>
+      <h1>SUA LISTA DE FAVORITOS</h1>
+      {
+        loading ? (
+          <LoadingDiv>
+            <p>Loading...</p>
+          </LoadingDiv>
+        ) : (
+          notFavorite ? (
+            <NotFavoritesDiv>
+              <h3>Você não tem personagens favoritos</h3>
+              <img src={notFavoritesImg} alt="Rick voando pelo espaço vazio que você deixaste" />
+            </NotFavoritesDiv>
+          ) : (
+            <Card characters={favorites} />
+        ))
+      }
       </FavoritesContainer>
-      
+
       <Footer />
     </div>
   )
--- src/pages/Favorites/styles.ts
+++ src/pages/Favorites/styles.ts
@@ -4,13 +4,48 @@ export const FavoritesContainer = styled.main`
   max-width: 1120px;
   margin: 0 auto;
   padding-top: 82px;
+  padding-left: 20px;
+  padding-right: 20px;
   padding-bottom: 60px;
 
   > h1 {
-    margin-top: 14px;
+    margin-top: 30px;
+    margin-bottom: 40px;
     font-size: 48px;
     font-weight: 700;
     color: #3cb54a;
     text-align: center;
+  }
+`;
+
+export const LoadingDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 40px;
+  margin-bottom: 60px;
+
+  > p {
+    font-size: 2.4rem;
+    color: #f2ec25;
+    margin: 0;
+  }
+`;
+
+export const NotFavoritesDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > h3 {
+    margin-top: 45px;
+    font-size: 2.2rem;
+    color: #f2ec25;
+    margin: 0;
+  }
+
+  > img {
+    margin-top: -45px;
+    
   }
 `;
